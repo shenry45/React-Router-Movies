78d1bbe62b775252c6846bcd94427a0ebfe162cd
shenry45 <shawn45henry@gmail.com>
2019-06-05 18:50:06 -0700

    add Link to home button in savedList

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -10,9 +10,7 @@ export default class Movie extends Component {
   }
 
   componentDidMount() {
-    // change this line to grab the id passed on the URL
     const id = this.props.match.params.id;
-    // props.match.params.id;
     this.fetchMovie(id);
   }
 
