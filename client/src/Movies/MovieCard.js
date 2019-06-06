231d3aa70ae7097936c087ffd9a2318b29ed90f5
shenry45 <shawn45henry@gmail.com>
2019-06-05 18:23:17 -0700

    init on Part 2 of Read Me

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const MovieCard = props => {
-  return;
+  return (
+    <a href={`/movies/${props.id}`}>
+    </a>
+  )
 };
 
 export default MovieCard;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default class MovieList extends Component {
@@ -34,7 +35,7 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <Link to={`/movies/${movie.id}`} className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -49,6 +50,6 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
-    </div>
+    </Link>
   );
 }
