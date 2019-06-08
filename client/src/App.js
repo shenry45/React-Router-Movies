7c4f14b726b964b43474aed9f878c351026d9a89
shenry45 <shawn45henry@gmail.com>
2019-06-07 18:17:53 -0700

    convert Saved Movie Link's into NavLink's

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,12 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      savedList: []
+      savedList: [],
+      id: undefined
     };
   }
 
-  addToSavedList = movie => {
+  addToSavedList = (movie, id) => {
     const savedList = this.state.savedList;
     savedList.push(movie);
     this.setState({ savedList });
@@ -22,7 +23,10 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <SavedList list={this.state.savedList} />
+        <SavedList
+          list={this.state.savedList}
+          id={this.state.id} 
+        />
         <Route
           path="/movies/:id"
           render={(props) => (
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -11,10 +11,10 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+          <NavLink to={`/movie/${this.props.id}`}className="saved-movie">{movie.title}</NavLink>
         ))}
         <div className="home-button">
-          <Link to="/">Home</Link>
+          <NavLink to="/">Home</NavLink>
         </div>
       </div>
     );
